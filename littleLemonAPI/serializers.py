cd1747b2a6d6c2ff1220abffceb735e199a2e711
Karim7651 <karimatef785@gmail.com>
2024-02-10 00:46:00 +0200

    update

--- littleLemonAPI/serializers.py
+++ littleLemonAPI/serializers.py
@@ -1,47 +1,72 @@
 from rest_framework import serializers
+from .models import MenuItem, Cart, Category, Order, OrderItem
 from django.contrib.auth.models import User
-
-from .models import *
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ["id", "title", "slug"]
+from django.core.exceptions import ValidationError
 
 
 class MenuItemSerializer(serializers.ModelSerializer):
-    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
 
-    
-    class Meta:
+    class Meta():
         model = MenuItem
-        fields = ["id", "title", "price", "category", "featured"]
+        fields = ['id','title','price','featured','category']
+        depth = 1
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta():
+        model = Category
+        fields = ['slug']
+
+class ManagerListSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = User
+        fields = ['id','username','email']
+
+class CartHelpSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = MenuItem
+        fields = ['id','title','price']
 
 class CartSerializer(serializers.ModelSerializer):
-    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), default=serializers.CurrentUserDefault())
-
-    class Meta:
+    menuitem = CartHelpSerializer()
+    class Meta():
         model = Cart
-        fields = ["user", "menuitem", "unit_price", "quantity", "price"]
-        extra_kwargs = {"price": {"read_only": True}}
+        fields = ['menuitem','quantity','price']
+        
+class CartAddSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = Cart
+        fields = ['menuitem','quantity']
+        extra_kwargs = {
+            'quantity': {'min_value': 1},
+        }
+class CartRemoveSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = Cart
+        fields = ['menuitem']
 
-
-class OrderItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OrderItem
-        fields = ["order", "menuitem", "quantity", "price"]
-
+class UserSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = User
+        fields = ['username']
 
 class OrderSerializer(serializers.ModelSerializer):
-    orderitem = OrderItemSerializer(many=True, read_only=True, source="order")
-
-    class Meta:
+    user = UserSerializer()
+    class Meta():
         model = Order
-        fields = ["id", "user", "delivery_crew", "status", "date", "total", "orderitem"]
+        fields = ['id','user','total','status','delivery_crew','date']
+
+class SingleHelperSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = MenuItem
+        fields = ['title','price']
+class SingleOrderSerializer(serializers.ModelSerializer):
+    menuitem = SingleHelperSerializer()
+    class Meta():
+        model = OrderItem
+        fields = ['menuitem','quantity']
 
 
-class UserSerilializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "username", "email"]
+class OrderPutSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = Order
+        fields = ['delivery_crew']
--- littleLemonAPI/views.py
+++ littleLemonAPI/views.py
@@ -1,146 +1,218 @@
 from rest_framework import generics
-from .serializers import *
-from .models import *
-from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
-from django.contrib.auth.models import User, Group
-from rest_framework.response import Response
-from decimal import Decimal
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound, JsonResponse
+from .models import MenuItem, Cart, Order, OrderItem, Category
+from .serializers import MenuItemSerializer, ManagerListSerializer, CartSerializer, OrderSerializer, CartAddSerializer, CartRemoveSerializer, SingleOrderSerializer, OrderPutSerializer, CategorySerializer
+from .paginations import MenuItemListPagination
 from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
-from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from django.contrib.auth.models import User, Group
+from .permissions import IsManager, IsDeliveryCrew
+import math
+from datetime import date
 
-
-class MenuItemView(generics.ListAPIView, generics.ListCreateAPIView):
+class MenuItemListView(generics.ListCreateAPIView):
+    throttle_classes = [AnonRateThrottle, UserRateThrottle]
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
-    ordering_fields = ['price']
-    search_fields = ['title']
-    throttle_classes = [AnonRateThrottle, UserRateThrottle]
+    search_fields = ['title','category__title']
+    ordering_fields=['price','category']
+    pagination_class = MenuItemListPagination
+
     def get_permissions(self):
-        if self.request.method == 'POST':
-            return [IsAdminUser()]
-        return [AllowAny()]
+        permission_classes = []
+        if self.request.method != 'GET':
+                permission_classes = [IsAuthenticated, IsAdminUser]
+        return[permission() for permission in permission_classes]
 
+class CategoryView(generics.ListCreateAPIView):
+    throttle_classes = [AnonRateThrottle, UserRateThrottle]
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+    permission_classes = [IsAdminUser]
 
-class SingleItemView(generics.RetrieveUpdateDestroyAPIView, generics.RetrieveAPIView):
+class MenuItemDetailView(generics.RetrieveUpdateDestroyAPIView):
+    throttle_classes = [AnonRateThrottle, UserRateThrottle]
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
-    throttle_classes = [AnonRateThrottle, UserRateThrottle]
+
     def get_permissions(self):
-        if self.request.method == 'POST' or self.request.method == 'PUT' or self.request.method == 'DELETE' or self.request.method == 'PATCH':
-            return [IsAdminUser()]
-        return [AllowAny()]
+        permission_classes = [IsAuthenticated]
+        if self.request.method == 'PATCH':
+            permission_classes = [IsAuthenticated, IsManager | IsAdminUser]
+        if self.request.method == "DELETE":
+            permission_classes = [IsAuthenticated, IsAdminUser]
+        return[permission() for permission in permission_classes]
+    
+    def patch(self, request, *args, **kwargs):
+        menuitem = MenuItem.objects.get(pk=self.kwargs['pk'])
+        menuitem.featured = not menuitem.featured
+        menuitem.save()
+        return JsonResponse(status=200, data={'message':'Featured status of {} changed to {}'.format(str(menuitem.title) ,str(menuitem.featured))})
 
 
-class ManagerUsersView(generics.ListCreateAPIView):
-    serializer_class = UserSerilializer
-    permission_classes = [IsAdminUser]
+class ManagersListView(generics.ListCreateAPIView):
+    throttle_classes = [AnonRateThrottle, UserRateThrottle]
+    queryset = User.objects.filter(groups__name='Managers')
+    serializer_class = ManagerListSerializer
+    permission_classes = [IsAuthenticated, IsManager | IsAdminUser]
 
-    def get_queryset(self):
-        # Get the 'manager' group
-        manager_group = Group.objects.get(name='manager')
-        # Get the users in the 'manager' group
-        queryset = User.objects.filter(groups=manager_group)
-        return queryset
+    def post(self, request, *args, **kwargs):
+        username = request.data['username']
+        if username:
+            user = get_object_or_404(User, username=username)
+            managers = Group.objects.get(name='Managers')
+            managers.user_set.add(user)
+            return JsonResponse(status=201, data={'message':'User added to Managers group'}) 
 
-    def perform_create(self, serializer):
-        # Assign the user to the 'manager' group
-        manager_group = Group.objects.get(name='manager')
-        user = serializer.save()
-        user.groups.add(manager_group)
+class ManagersRemoveView(generics.DestroyAPIView):
+    throttle_classes = [AnonRateThrottle, UserRateThrottle]
+    serializer_class = ManagerListSerializer
+    permission_classes = [IsAuthenticated, IsManager | IsAdminUser]
+    queryset = User.objects.filter(groups__name='Managers')
 
+    def delete(self, request, *args, **kwargs):
+        pk = self.kwargs['pk']
+        user = get_object_or_404(User, pk=pk)
+        managers = Group.objects.get(name='Managers')
+        managers.user_set.remove(user)
+        return JsonResponse(status=200, data={'message':'User removed Managers group'})
 
-class ManagerSingleUserView(generics.RetrieveDestroyAPIView):
-    serializer_class = UserSerilializer
-    permission_classes = [IsAdminUser]
+class DeliveryCrewListView(generics.ListCreateAPIView):
+    throttle_classes = [AnonRateThrottle, UserRateThrottle]
+    queryset = User.objects.filter(groups__name='Delivery crew')
+    serializer_class = ManagerListSerializer
+    permission_classes = [IsAuthenticated, IsManager | IsAdminUser]
 
-    def get_queryset(self):
-        # Get the 'manager' group
-        manager_group = Group.objects.get(name='manager')
-        # Get the users in the 'manager' group
-        queryset = User.objects.filter(groups=manager_group)
-        return queryset
+    def post(self, request, *args, **kwargs):
+        username = request.data['username']
+        if username:
+            user = get_object_or_404(User, username=username)
+            crew = Group.objects.get(name='Delivery crew')
+            crew.user_set.add(user)
+            return JsonResponse(status=201, data={'message':'User added to Delivery Crew group'})
 
+class DeliveryCrewRemoveView(generics.DestroyAPIView):
+    throttle_classes = [AnonRateThrottle, UserRateThrottle]
+    serializer_class = ManagerListSerializer
+    permission_classes = [IsAuthenticated, IsManager | IsAdminUser]
+    queryset = User.objects.filter(groups__name='Delivery crew')
 
-class Delivery_crew_management(generics.ListCreateAPIView):
-    serializer_class = UserSerilializer
-    permission_classes = [IsAdminUser]
+    def delete(self, request, *args, **kwargs):
+        pk = self.kwargs['pk']
+        user = get_object_or_404(User, pk=pk)
+        managers = Group.objects.get(name='Delivery crew')
+        managers.user_set.remove(user)
+        return JsonResponse(status=201, data={'message':'User removed from the Delivery crew group'})
 
-    def get_queryset(self):
-        delivery_group = Group.objects.get(name='delivery crew')
-        queryset = User.objects.filter(groups=delivery_group)
-        return queryset
-
-    def perform_create(self, serializer):
-        delivery_group = Group.objects.get(name='delivery crew')
-        user = serializer.save()
-        user.groups.add(delivery_group)
-
-
-class Delivery_crew_management_single_view(generics.RetrieveDestroyAPIView):
-    serializer_class = UserSerilializer
-    permission_classes = [IsAdminUser]
-
-    def get_queryset(self):
-        delivery_group = Group.objects.get(name='delivery crew')
-        queryset = User.objects.filter(groups=delivery_group)
-        return queryset
-
-
-class Customer_Cart(generics.ListCreateAPIView):
+class CartOperationsView(generics.ListCreateAPIView):
+    throttle_classes = [AnonRateThrottle, UserRateThrottle]
     serializer_class = CartSerializer
     permission_classes = [IsAuthenticated]
-    def get_queryset(self):
-        user = self.request.user
-        return Cart.objects.filter(user=user)
 
-    def perform_create(self, serializer):
-        menuitem = self.request.data.get('menuitem')
-        quantity = self.request.data.get('quantity')
-        unit_price = MenuItem.objects.get(pk=menuitem).price
-        quantity = int(quantity)
-        price = quantity * unit_price
-        serializer.save(user=self.request.user, price=price)
+    def get_queryset(self, *args, **kwargs):
+        cart = Cart.objects.filter(user=self.request.user)
+        return cart
 
-    def delete(self, request):
-        user = self.request.user
-        Cart.objects.filter(user=user).delete()
-        return Response(status=204)
+    def post(self, request, *arg, **kwargs):
+        serialized_item = CartAddSerializer(data=request.data)
+        serialized_item.is_valid(raise_exception=True)
+        id = request.data['menuitem']
+        quantity = request.data['quantity']
+        item = get_object_or_404(MenuItem, id=id)
+        price = int(quantity) * item.price
+        try:
+            Cart.objects.create(user=request.user, quantity=quantity, unit_price=item.price, price=price, menuitem_id=id)
+        except:
+            return JsonResponse(status=409, data={'message':'Item already in cart'})
+        return JsonResponse(status=201, data={'message':'Item added to cart!'})
 
 
-class Orders_view(generics.ListCreateAPIView):
-    serializer_class = OrderSerializer
-    permission_classes = [IsAuthenticated]
+    def delete(self, request, *arg, **kwargs):
+        if request.data['menuitem']:
+            serialized_item = CartRemoveSerializer(data=request.data)
+            serialized_item.is_valid(raise_exception=True)
+            menuitem = request.data['menuitem']
+            cart = get_object_or_404(Cart, user=request.user, menuitem=menuitem )
+            cart.delete()
+            return JsonResponse(status=200, data={'message':'Item removed from cart'})
+        else:
+            Cart.objects.filter(user=request.user).delete()
+            return JsonResponse(status=201, data={'message':'All Items removed from cart'})
+
+class OrderOperationsView(generics.ListCreateAPIView):
     throttle_classes = [AnonRateThrottle, UserRateThrottle]
-    def perform_create(self, serializer):
-        cart_items = Cart.objects.filter(user=self.request.user)
-        total = self.calculate_total(cart_items)
-        order = serializer.save(user=self.request.user, total=total)
-
-        for cart_item in cart_items:
-            OrderItem.objects.create(menuitem=cart_item.menuitem, quantity=cart_item.quantity,
-                                     unit_price=cart_item.unit_price, price=cart_item.price, order=order)
-            cart_item.delete()
-
-    def get_queryset(self):
-        user = self.request.user
-        if user.groups.filter(name='manager').exists():
-            return Order.objects.all()
-        return Order.objects.filter(user=user)
-
-
-    def calculate_total(self, cart_items):
-        total = Decimal(0)
-        for item in cart_items:
-            total += item.price
-        return total
-
-
-class Single_Order_view(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = OrderSerializer
-    permission_classes = [IsAuthenticated]
-    def get_queryset(self):
-        user = self.request.user
-        if user.groups.filter(name='manager').exists():
-            return Order.objects.all()
-        return Order.objects.filter(user=user)
+        
+    def get_queryset(self, *args, **kwargs):
+        if self.request.user.groups.filter(name='Managers').exists() or self.request.user.is_superuser == True :
+            query = Order.objects.all()
+        elif self.request.user.groups.filter(name='Delivery crew').exists():
+            query = Order.objects.filter(delivery_crew=self.request.user)
+        else:
+            query = Order.objects.filter(user=self.request.user)
+        return query
 
+    def get_permissions(self):
+        
+        if self.request.method == 'GET' or 'POST' : 
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsAuthenticated, IsManager | IsAdminUser]
+        return[permission() for permission in permission_classes]
+
+    def post(self, request, *args, **kwargs):
+        cart = Cart.objects.filter(user=request.user)
+        x=cart.values_list()
+        if len(x) == 0:
+            return HttpResponseBadRequest()
+        total = math.fsum([float(x[-1]) for x in x])
+        order = Order.objects.create(user=request.user, status=False, total=total, date=date.today())
+        for i in cart.values():
+            menuitem = get_object_or_404(MenuItem, id=i['menuitem_id'])
+            orderitem = OrderItem.objects.create(order=order, menuitem=menuitem, quantity=i['quantity'])
+            orderitem.save()
+        cart.delete()
+        return JsonResponse(status=201, data={'message':'Your order has been placed! Your order number is {}'.format(str(order.id))})
+
+class SingleOrderView(generics.ListCreateAPIView):
+    throttle_classes = [AnonRateThrottle, UserRateThrottle]
+    serializer_class = SingleOrderSerializer
+    
+    def get_permissions(self):
+        order = Order.objects.get(pk=self.kwargs['pk'])
+        if self.request.user == order.user and self.request.method == 'GET':
+            permission_classes = [IsAuthenticated]
+        elif self.request.method == 'PUT' or self.request.method == 'DELETE':
+            permission_classes = [IsAuthenticated, IsManager | IsAdminUser]
+        else:
+            permission_classes = [IsAuthenticated, IsDeliveryCrew | IsManager | IsAdminUser]
+        return[permission() for permission in permission_classes] 
+
+    def get_queryset(self, *args, **kwargs):
+            query = OrderItem.objects.filter(order_id=self.kwargs['pk'])
+            return query
+
+
+    def patch(self, request, *args, **kwargs):
+        order = Order.objects.get(pk=self.kwargs['pk'])
+        order.status = not order.status
+        order.save()
+        return JsonResponse(status=200, data={'message':'Status of order #'+ str(order.id)+' changed to '+str(order.status)})
+
+    def put(self, request, *args, **kwargs):
+        serialized_item = OrderPutSerializer(data=request.data)
+        serialized_item.is_valid(raise_exception=True)
+        order_pk = self.kwargs['pk']
+        crew_pk = request.data['delivery_crew'] 
+        order = get_object_or_404(Order, pk=order_pk)
+        crew = get_object_or_404(User, pk=crew_pk)
+        order.delivery_crew = crew
+        order.save()
+        return JsonResponse(status=201, data={'message':str(crew.username)+' was assigned to order #'+str(order.id)})
+
+    def delete(self, request, *args, **kwargs):
+        order = Order.objects.get(pk=self.kwargs['pk'])
+        order_number = str(order.id)
+        order.delete()
+        return JsonResponse(status=200, data={'message':'Order #{} was deleted'.format(order_number)})
