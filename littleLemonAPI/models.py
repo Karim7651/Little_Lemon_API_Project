87d3199c907ad799f9359c8bd3d60b482fb8e497
Karim7651 <karimatef785@gmail.com>
2024-02-09 12:33:49 +0200

    models update, serializers

--- littleLemonAPI/models.py
+++ littleLemonAPI/models.py
@@ -4,12 +4,16 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     slug=models.SlugField()
     title=models.CharField(max_length=255,db_index=True)
+    def __str__(self):
+        return self.title
 
 class MenuItem(models.Model):
     title = models.CharField(max_length=255,db_index=True)
     price = models.DecimalField(max_digits=6,decimal_places=2,db_index=True)
     featured = models.BooleanField(db_index=True)
     category = models.ForeignKey(Category,on_delete=models.PROTECT)
+    def __str__(self):
+        return f"{self.title} - {self.price}"
     
 class Cart(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
@@ -20,6 +24,8 @@ class Cart(models.Model):
     
     class Meta:
         unique_together=('menuitem','user') #user can add item only once to his cart
+    def __str__(self):
+        return f"{self.user.username}'s Cart - {self.menuitem.title}"
         
 class Order(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
@@ -27,6 +33,8 @@ class Order(models.Model):
     status = models.BooleanField(db_index=True,default=0)
     total = models.DecimalField(max_digits=6,decimal_places=2)
     date = models.DateField(db_index=True)
+    def __str__(self):
+        return f"Order by {self.user.username} on {self.date}"
     
 class OrderItem(models.Model):
     order = models.ForeignKey(Order,on_delete=models.CASCADE)
@@ -35,7 +43,15 @@ class OrderItem(models.Model):
     unit_price = models.DecimalField(max_digits=6,decimal_places=2)
     price = models.DecimalField(max_digits=6,decimal_places=2)
     
+    def save(self, *args, **kwargs):
+        # Calculate the total price before saving
+        self.price = self.unit_price * self.quantity
+        super().save(*args, **kwargs)
+    
     class meta:
         unique_together=('order','menuitem') #menu item can be added once only in the same orderItem with quantity
+        
+    def __str__(self):
+        return f"Item: {self.menuitem.title} - Quantity: {self.quantity} - Price: {self.price}"
 
         
