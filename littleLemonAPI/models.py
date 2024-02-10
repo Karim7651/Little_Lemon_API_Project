bc81f2813802f66bb595025af4d7019a0ee16f46
Karim7651 <karimatef785@gmail.com>
2024-02-10 05:26:24 +0200

    final updatee

--- littleLemonAPI/models.py
+++ littleLemonAPI/models.py
@@ -1,57 +1,45 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+
+
 class Category(models.Model):
-    slug=models.SlugField()
-    title=models.CharField(max_length=255,db_index=True)
-    def __str__(self):
-        return self.title
+    slug = models.SlugField()
+    title = models.CharField(max_length=255, db_index=True)
+
 
 class MenuItem(models.Model):
-    title = models.CharField(max_length=255,db_index=True)
-    price = models.DecimalField(max_digits=6,decimal_places=2,db_index=True)
+    title = models.CharField(max_length=255, db_index=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
     featured = models.BooleanField(db_index=True)
-    category = models.ForeignKey(Category,on_delete=models.PROTECT)
-    def __str__(self):
-        return f"{self.title} - {self.price}"
-    
-class Cart(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    menuitem = models.ForeignKey(MenuItem,on_delete=models.CASCADE)
-    quantity = models.SmallIntegerField
-    unit_price = models.DecimalField(max_digits=6,decimal_places=2)
-    price = models.DecimalField(max_digits=6,decimal_places=2)
-    
-    class Meta:
-        unique_together=('menuitem','user') #user can add item only once to his cart
-    def __str__(self):
-        return f"{self.user.username}'s Cart - {self.menuitem.title}"
-        
-class Order(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    delivery_crew = models.ForeignKey(User,on_delete=models.SET_NULL,related_name='delivery_crew',null=True) #since user and del.crew refer to the same Table we have to make foreign key that way
-    status = models.BooleanField(db_index=True,default=0)
-    total = models.DecimalField(max_digits=6,decimal_places=2)
-    date = models.DateField(db_index=True)
-    def __str__(self):
-        return f"Order by {self.user.username} on {self.date}"
-    
-class OrderItem(models.Model):
-    order = models.ForeignKey(Order,on_delete=models.CASCADE)
-    menuitem = models.ForeignKey(MenuItem,on_delete=models.CASCADE)
-    quantity = models.SmallIntegerField()
-    unit_price = models.DecimalField(max_digits=6,decimal_places=2)
-    price = models.DecimalField(max_digits=6,decimal_places=2)
-    
-    def save(self, *args, **kwargs):
-        # Calculate the total price before saving
-        self.price = self.unit_price * self.quantity
-        super().save(*args, **kwargs)
-    
-    class meta:
-        unique_together=('order','menuitem') #menu item can be added once only in the same orderItem with quantity
-        
-    def __str__(self):
-        return f"Item: {self.menuitem.title} - Quantity: {self.quantity} - Price: {self.price}"
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
 
-        
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = ('menuitem', 'user')
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    delivery_crew = models.ForeignKey(
+        User, on_delete=models.SET_NULL, related_name="delivery_crew", null=True)
+    status = models.BooleanField(default=0, db_index=True)
+    total = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    date = models.DateField(db_index=True)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name='order')
+    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = ('order', 'menuitem')
--- littleLemonAPI/serializers.py
+++ littleLemonAPI/serializers.py
@@ -1,72 +1,62 @@
 from rest_framework import serializers
-from .models import MenuItem, Cart, Category, Order, OrderItem
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
+from decimal import Decimal
+
+from .models import Category, MenuItem, Cart, Order, OrderItem
+
+
+class CategorySerializer (serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'title', 'slug']
 
 
 class MenuItemSerializer(serializers.ModelSerializer):
-
-    class Meta():
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all()
+    )
+    # category = CategorySerializer(read_only=True)
+    class Meta:
         model = MenuItem
-        fields = ['id','title','price','featured','category']
-        depth = 1
+        fields = ['id', 'title', 'price', 'category', 'featured']
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta():
-        model = Category
-        fields = ['slug']
-
-class ManagerListSerializer(serializers.ModelSerializer):
-    class Meta():
-        model = User
-        fields = ['id','username','email']
-
-class CartHelpSerializer(serializers.ModelSerializer):
-    class Meta():
-        model = MenuItem
-        fields = ['id','title','price']
 
 class CartSerializer(serializers.ModelSerializer):
-    menuitem = CartHelpSerializer()
-    class Meta():
-        model = Cart
-        fields = ['menuitem','quantity','price']
-        
-class CartAddSerializer(serializers.ModelSerializer):
-    class Meta():
-        model = Cart
-        fields = ['menuitem','quantity']
-        extra_kwargs = {
-            'quantity': {'min_value': 1},
-        }
-class CartRemoveSerializer(serializers.ModelSerializer):
-    class Meta():
-        model = Cart
-        fields = ['menuitem']
+    user = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        default=serializers.CurrentUserDefault()
+    )
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta():
-        model = User
-        fields = ['username']
+
+    def validate(self, attrs):
+        attrs['price'] = attrs['quantity'] * attrs['unit_price']
+        return attrs
+
+    class Meta:
+        model = Cart
+        fields = ['user', 'menuitem', 'unit_price', 'quantity', 'price']
+        extra_kwargs = {
+            'price': {'read_only': True}
+        }
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['order', 'menuitem', 'quantity', 'price']
+
 
 class OrderSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
-    class Meta():
+
+    orderitem = OrderItemSerializer(many=True, read_only=True, source='order')
+
+    class Meta:
         model = Order
-        fields = ['id','user','total','status','delivery_crew','date']
-
-class SingleHelperSerializer(serializers.ModelSerializer):
-    class Meta():
-        model = MenuItem
-        fields = ['title','price']
-class SingleOrderSerializer(serializers.ModelSerializer):
-    menuitem = SingleHelperSerializer()
-    class Meta():
-        model = OrderItem
-        fields = ['menuitem','quantity']
+        fields = ['id', 'user', 'delivery_crew',
+                  'status', 'date', 'total', 'orderitem']
 
 
-class OrderPutSerializer(serializers.ModelSerializer):
-    class Meta():
-        model = Order
-        fields = ['delivery_crew']
+class UserSerilializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','username','email']
